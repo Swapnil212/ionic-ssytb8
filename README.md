5c79ca8d5c1f6b3133eb3a83f9d1b9ce04b51ee1
Swapnil212 <swap212@gmail.com>
2020-11-20 20:49:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-ssytb8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-ssytb8)
---
+++ pages/chat/chat.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import '../../assets/js/bootstrap.min.js';
+import '../../assets/js/jquery-3.3.1.slim.min.js';
+import '../../assets/js/swipe.min.js';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
